5094f02651c87915c09b8222a978869786c380e2
Sreenidhi <47133316+Sree27-Shanger@users.noreply.github.com>
2020-10-05 12:35:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function changeBackgroundColor(
   selector = "body",
-  defaultStart = 40,
+  default = 40,
   defaultEnd = 256,
   interval = 50
 ) {
   const background = document.querySelector(selector);
-  let i = defaultStart;
+  let i = default;
   let n = defaultEnd;
   setInterval(() => {
     if (i < defaultEnd) {
-      background.style.backgroundColor = `rgb(${i},${i - defaultStart},158)`;
+      background.style.backgroundColor = `rgb(${i},${i - default},158)`;
       i++;
       if (i === defaultEnd - 1) {
         n = defaultEnd;
       }
-    } else if (n > defaultStart) {
-      background.style.backgroundColor = `rgb(${n},${n - defaultStart},158)`;
+    } else if (n > default) {
+      background.style.backgroundColor = `rgb(${n},${n - default},158)`;
       n--;
-      if (n === defaultStart + 1) {
-        i = defaultStart;
+      if (n === default + 1) {
+        i = default;
       }
     }
   }, interval);
